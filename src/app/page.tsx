3cbdd78dcd7b230d07e7cff50b961ab8ca527324
GlitchUser001 <sheronkhoya@gmail.com>
2026-08-13 08:21:06 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
-"use client";
-
-import { useState } from "react";
-
-import Intro from "@/components/Intro";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Education from "@/components/Education";
 
 export default function Home() {
-  const [introComplete, setIntroComplete] = useState(false);
-
   return (
     <main className="bg-[#050505] text-[#f5f5f7]">
-      {!introComplete ? (
-        <Intro
-          onComplete={() => {
-            setIntroComplete(true);
-          }}
-        />
-      ) : (
-        <>
-          <Hero />
-          <About />
-          <Education />
-        </>
-      )}
+      <Hero />
+      <About />
+      <Education />
     </main>
   );
 }
